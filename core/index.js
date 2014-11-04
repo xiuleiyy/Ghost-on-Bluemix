aab501dd1447aab8fff49119ffb098c8a503961c
acostry <zhuxiulei@gmail.com>
2014-11-04 17:28:10 +0800

    upload app files

---
+++ core/index.js
@@ -0,0 +1,16 @@
+// # Ghost bootloader
+// Orchestrates the loading of Ghost
+// When run from command line.
+
+var server = require('./server');
+
+//process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+function makeGhost(options) {
+    options = options || {};
+
+    return server(options);
+}
+
+module.exports = makeGhost;
